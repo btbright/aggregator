744d078b60caeaa5775d4eb99a465c79d2962064
btbright <btbright@gmail.com>
2015-11-14 23:50:57 -0600

    updated twitter handle

--- common/components/InstructionsModal.jsx
+++ common/components/InstructionsModal.jsx
@@ -16,7 +16,7 @@ class InstructionsModal extends Component {
 						<p>If you are the first person to click a thing in chat to make it active and then other people click it to support it, you&rsquo;ll get points for finding it. But, if you click a thing in chat and then no one else clicks it, you&rsquo;ll lose points. So, don&rsquo;t click bad things.</p>
 						<p>When you click a thing that is already active, you are supporting it in its rise to gold glory, but you can only support one thing at a time. So, pick the best thing. When you are supporting something there will be a small dot to its left. The more people support a thing, the higher the bar will go.</p>
 						<p>If you have the most points at the end of the game, that&rsquo;s good. You win.</p>
-						<p>This is a testing release, so things may fall apart at any time. My humblest apologies if/when it does. Please let me know if you have any questions, suggestions, comments, diatribes, etc. I&rsquo;d love hear what you think: <a target='_blank' href='https://twitter.com/btbright'>@btbright</a></p>
+						<p>This is a testing release, so things may fall apart at any time. My humblest apologies if/when it does. Please let me know if you have any questions, suggestions, comments, diatribes, etc. I&rsquo;d love hear what you think: <a target='_blank' href='https://twitter.com/aggbuzz'>@aggbuzz</a></p>
 						<p>Grey &lt; Green &lt; Blue &lt; Purple &lt; Gold</p>
 						<p>TL;DR - Be funny in chat. Click things you like. Points will flow.</p>
 					</div>
